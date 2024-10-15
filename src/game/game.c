722ab387e829c9d2a764ee33fe1281cfb214b9fb
Arthur Pernot <apernot@made-f0Br6s5.clusters.42paris.fr>
2024-10-15 13:57:17 +0200

    merge

--- src/game/game.c
+++ src/game/game.c
@@ -6,7 +6,7 @@
 /*   By: apernot <apernot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:42:24 by yohurteb          #+#    #+#             */
-/*   Updated: 2024/10/15 13:50:49 by apernot          ###   ########.fr       */
+/*   Updated: 2024/10/15 13:54:47 by apernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ void	game(t_data *data)
 	t_player player;
 	ft_memset(&player, 0, sizeof(t_player));
 
-	player.dir.x = -1;
-	player.dir.y = 0;
-	player.plane.x = 0;
-	player.plane.y = 0.66;
-	player.pos.x = 22;
-	player.pos.y = 12;
+	
+	player.dir->x = -1;
+	player.dir->y = 0;
+	player.plane->x = 0;
+	player.plane->y = 0.66;
+	player.pos->x = 22;
+	player.pos->y = 12;
 	raycasting(data, &player);
 }
